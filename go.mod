e446fac8295cc362ea639a35d2fb775d642feb9a
mbellotti <marianne.bellotti@gmail.com>
2020-03-01 02:11:28 -0500

    move config file to an accessible directory

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/dghubble/go-twitter v0.0.0-20190719072343-39e5462e111f
 	github.com/dghubble/oauth1 v0.6.0
 	github.com/manifoldco/promptui v0.7.0
+	github.com/mitchellh/go-homedir v1.1.0
 	github.com/spf13/cobra v0.0.6
 	github.com/spf13/viper v1.6.2
 )
--- main.go
+++ main.go
@@ -2,18 +2,37 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/mbellotti/netback/cmd"
+	"github.com/mitchellh/go-homedir"
 	_ "github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
 func main() {
+	h, err := homedir.Dir()
+	configpath := fmt.Sprintf("%s/netback", h)
+	if _, err := os.Stat(configpath); os.IsNotExist(err) {
+		err := os.Mkdir(configpath, 0755)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+	filepath := fmt.Sprintf("%s/config.yaml", configpath)
+	if _, err := os.Stat(filepath); os.IsNotExist(err) {
+		_, err = os.Create(filepath)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+
 	viper.SetConfigName("config") // name of config file (without extension)
 	viper.SetConfigType("yaml")
-	viper.AddConfigPath(".") // path to look for the config file in
+	viper.AddConfigPath(configpath) // path to look for the config file in
 
-	err := viper.ReadInConfig()
+	fmt.Println(viper.ConfigFileUsed())
+	err = viper.ReadInConfig()
 	if err != nil {
 		fmt.Println("Configuration not found, please run `netback config` to set up")
 	} else {
